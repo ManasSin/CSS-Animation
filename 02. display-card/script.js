75682307be891fda4d2af9ec3a9a037189c65e79
ManasSin <maltisadan12@gmail.com>
2023-01-20 00:28:19 +0530

    done 90%, RWT

---
+++ 02. display-card/script.js
@@ -0,0 +1,63 @@
+const imageCards = [
+  {
+    imgesrc: "./images/cloudy-weather.jpg",
+    alt: "cloudy weather",
+  },
+  {
+    imgesrc: "./images/cool-boy.jpg",
+    alt: "cool boy",
+  },
+  {
+    imgesrc: "./images/dreamy-desk.jpg",
+    alt: "Dreamy desk",
+  },
+  {
+    imgesrc: "./images/imaginary-women.jpg",
+    alt: "Imaginary women",
+  },
+  {
+    imgesrc: "./images/lonly-guy.jpg",
+    alt: "lonly boy",
+  },
+  {
+    imgesrc: "./images/mesmering-view.jpg",
+    alt: "mesmering view",
+  },
+];
+
+const mainImage = document.querySelector(".main-img");
+
+let cardContainer = document.querySelector(".below-card");
+
+imageCards.forEach((element) => {
+  let smallCards = document.createElement("div");
+  smallCards = `
+    <div class=inner-cards >
+        <img
+        src="${element.imgesrc}"
+        alt="${element.alt}"
+        class="small-images"
+        />
+    </div>
+    `;
+  cardContainer.innerHTML += smallCards;
+});
+
+const cards = document.querySelectorAll(".small-images");
+console.log(cards);
+
+// let imageHolder = document.querySelector(".inner-cards").childNodes[1];
+
+// cards.addEventListener("click", function () {
+//   mainImage.attributes.src.nodeValue = `${imageHolder.attributes.src.nodeValue}`;
+
+//   document.querySelector(".inner-cards").classList.toggle("hidden");
+// });
+
+cards.forEach((element) => {
+  element.addEventListener("click", function () {
+    mainImage.attributes.src.nodeValue = `${element.attributes.src.nodeValue}`;
+    document.querySelector(".inner-cards").classList.toggle("hidden");
+    document.querySelector("#heading-text").classList.add("hidden");
+  });
+});

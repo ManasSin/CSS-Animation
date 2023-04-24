61a5faf10e862f81f72cd4d0a8aceaf270a75e72
ManasSin <maltisadan12@gmail.com>
2023-04-24 23:19:39 +0530

    start over

--- 11. animating-Dialoge-Element/file-upload/src/App.jsx
+++ 11. animating-Dialoge-Element/file-upload/src/App.jsx
@@ -1,9 +1,16 @@
+import { useState, useRef, useEffect } from "react";
 import { AddFileBtn, File } from "./components";
 function App() {
   const [dialogeOpen, setDialogeOpen] = useState(false);
-  const handleMegaDialoge = () => {
-    setDialogeOpen((Open) => !Open);
-  };
+  const myRef = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((visibleEntries) => {
+      const visibles = visibleEntries[0];
+      console.log("visible", visibles.isIntersecting);
+    });
+    observer.observe(myRef.current);
+  }, []);
   return (
     <main className="w-full h-[100vh] flex flex-col flex-wrap items-stretch justify-end col sm:items-center sm:justify-center pb-32 sm:w-fit sm:mx-auto gap-3">
       <File />
@@ -11,14 +18,55 @@ function App() {
       <File />
       <File />
 
-      <AddFileBtn />
-      {dialogeOpen ? (
-        <dialog>
-          <form action="">
-            <label htmlFor="">kuchbhi</label>
-          </form>
-        </dialog>
-      ) : null}
+      <AddFileBtn setDialogeOpen={setDialogeOpen} />
+      <div
+        role="dialoge"
+        ref={myRef}
+        className={`
+          w-fit 
+          h-fit 
+          translate-x-0 
+          absolute 
+          top-2 
+        `}
+      >
+        {/* ${
+          dialogeOpen
+            ? "w-[100%] h-[100%] translate-x-0"
+            : "w-0 h-0 translate-x-full"
+        } */}
+        <form
+          action=""
+          className="
+            grid
+            col-span-1
+            row-span[auto_auto_auto]
+          "
+        >
+          <header
+            className="
+          col-span-full
+        "
+          >
+            <h2>heading for dialoge</h2>
+          </header>
+          <section
+            className="
+          col-span-full
+        "
+          >
+            <label htmlFor="input"></label>
+            <input type="file" name="input" />
+          </section>
+          <footer
+            className="
+          col-span-full
+        "
+          >
+            <button>submit</button>
+          </footer>
+        </form>
+      </div>
     </main>
   );
 }
--- 11. animating-Dialoge-Element/file-upload/src/components/AddFileBtn.jsx
+++ 11. animating-Dialoge-Element/file-upload/src/components/AddFileBtn.jsx
@@ -1,8 +1,14 @@
 import { FiPlus } from "react-icons/fi";
 
-const AddFileBtn = () => {
+const AddFileBtn = ({ setDialogeOpen }) => {
+  const handleMegaDialoge = () => {
+    setDialogeOpen((Open) => !Open);
+  };
   return (
-    <button className="text-white font-bold text-3xl p-5 rounded-full drop-shadow-lg shadow-orange-950 bg-black hover:-translate-y-1 transition absolute left-[5%] bottom-[5%]">
+    <button
+      className="text-white font-bold text-3xl p-5 rounded-full drop-shadow-lg shadow-orange-950 bg-black hover:-translate-y-1 transition absolute left-[5%] bottom-[5%]"
+      onClick={handleMegaDialoge}
+    >
       <FiPlus width={"30px"} />
     </button>
   );
